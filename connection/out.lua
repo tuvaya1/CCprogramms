a521bae952019518c72a7280e7cae0e6024c25ea
tuvaya1 <tuvayavania1@gmail.com>
2026-08-17 14:31:45 +0700

    Update out.lua

--- connection/out.lua
+++ connection/out.lua
@@ -4,14 +4,16 @@ local codes = require("codes")
 local OUT = {}
 
 local tick
+local speed = 40
 
 function sendChar(char)
 	local c = codes.ltc[char];
     	if not c then return end
-
+    
 	for i = 1, #c do
 		rs.setOutput(io.OUT_DATA, c:sub(i,i) == "-")
-		rs.setOutput("left", tick)
+    os.sleep(1/speed);
+        rs.setOutput(io.OUT_SYNTH, not tick)
 		tick = not tick
         	os.sleep(1 / speed)
 	end
@@ -21,13 +23,13 @@ function OUT.print(input)
 	tick = rs.getOutput(io.OUT_SYNTH)
 	rs.setOutput(io.OUT_DATA, false)
 	
-	for i = 1, #str do
-		sendChar(str:sub(i,i))
+	for i = 1, #input do
+		sendChar(input:sub(i,i))
 	end
 end
 
 function OUT.println(input)
-	print(inpit .. "&")
+	OUT.print(input .. "&")
 end
 
 return OUT
