f30eb20acf4275f6485f36c817978ee2b6021328
tuvaya1 <tuvayavania1@gmail.com>
2026-08-18 17:11:59 +0700

    Update in.lua

--- connection/in.lua
+++ connection/in.lua
@@ -1,8 +1,8 @@
 local io = require("Connect_lib.IO_settings")
 local codes = require("Connect_lib.codes")
-
+ 
 local IN = {}
-
+ 
 function toString(input)
   local s = ""
   local len = #input
@@ -13,28 +13,29 @@ function toString(input)
   end
   return s
 end
-
+ 
 function IN.readLine()
-	local last = rs.getInput(io.IN_SYNTH)
-	local input = ""
-	while true do
-		local event= os.pullEvent("redstone")
-		if rs.getInput(io.IN_SYNTH) ~= last then
-			last = rs.getInput(io.IN_SYNTH)
-			if (rs.getInput(io.IN_DATA)) then
-				input = input .. "-"
-			else 
-				input = input .. "."
-			end
-		end
-		if #input > 0 and #input % 8 == 0 then 
-	  		local decoded = toString(input)
-	  		if decoded:sub(-1) == "&" then
-	   			return (decoded:sub(1, -2))
-	  		end
-		end
-
-	end
+    local last = rs.getInput(io.IN_SYNTH)
+    local input = ""
+    while true do
+        local event= os.pullEvent("redstone")
+        if rs.getInput(io.IN_SYNTH) ~= last then
+            last = rs.getInput(io.IN_SYNTH)
+            if (rs.getInput(io.IN_DATA)) then
+                input = input .. "-"
+            else 
+                input = input .. "."
+            end
+        end
+        if #input > 0 and #input % 8 == 0 then 
+            local decoded = toString(input)
+            if decoded:sub(-1) == "&" then
+                return (decoded:sub(1, -2))
+            end
+        end
+ 
+    end
 end
-
+ 
 return IN
+ 
